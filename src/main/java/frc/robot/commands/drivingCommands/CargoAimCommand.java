9ab1cf9a0d6241d859605a411c6e00725847f77a
Eden Brown <edenbrown828@gmail.com>
2022-01-22 15:49:32 -0600

    Create CargoAimCommand.java

---
+++ src/main/java/frc/robot/commands/drivingCommands/CargoAimCommand.java
@@ -0,0 +1,18 @@
+package frc.robot.commands.drivingCommands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.operatorInputs.Controls;
+import frc.robot.subsystems.drive.Drive;
+
+public class CargoAimCommand extends CommandBase{
+    
+    private Drive drive;
+    private Controls controls;
+    
+    
+    public CargoAimCommand(Drive drive, Controls controls) {
+        this.drive = drive;
+        this.controls = controls;
+        addRequirements(drive);
+    }
+}
