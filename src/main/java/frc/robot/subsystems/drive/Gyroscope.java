a93552af15f5d4a1f12d22832a94652eeac9c4eb
chase beverly <89940965+Robochase21@users.noreply.github.com>
2022-01-27 17:57:48 -0600

    reformatted gyroscope and added some methods (#23)

--- src/main/java/frc/robot/subsystems/drive/Gyroscope.java
+++ src/main/java/frc/robot/subsystems/drive/Gyroscope.java
@@ -1,40 +1,47 @@
 package frc.robot.subsystems.drive;
 
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Timer;
-//import com.analog.adis16470.frc.ADIS16470_IMU;
-//import com.analog.adis16470.frc.ADIS16470_IMU.IMUAxis;
+import com.kauailabs.navx.frc.AHRS;
 
 public class Gyroscope {
     //Classes
-    //private ADIS16470_IMU imu;
+    private AHRS ahrs; 
     //variables
     private double startTime;
     private double driftPerSecond;
     
     public Gyroscope(){     
-        //this.imu = new ADIS16470_IMU();
-        //this.imu.setYawAxis(IMUAxis.kY);
+        this.ahrs = new AHRS(SPI.Port.kMXP);
         this.calibrate();
     }
 
     public double getGyroAngle(){
         double runTime = Timer.getFPGATimestamp() - startTime;
         double drift = runTime * driftPerSecond;
-        return 0;//this.imu.getAngle() - drift;
+        return this.ahrs.getAngle() - drift;
+    }
+
+    public void getRotation2d(){
+        this.ahrs.getRotation2d();
+    }
+
+    public void getRate(){
+        this.ahrs.getRate();
     }
 
     public void resetGyro(){
-        //this.imu.reset();
+        this.ahrs.reset();
         this.startTime = Timer.getFPGATimestamp();
     }
 
     public void calibrate() {
         this.startTime = Timer.getFPGATimestamp();   
-        //double startAngle = imu.getAngle();
+        double startAngle = ahrs.getAngle();
         try{
             Thread.sleep(1000);
         } catch(Exception e) {
         }
-        //this.driftPerSecond = (imu.getAngle() - startAngle)/(Timer.getFPGATimestamp() - startTime);
+        this.driftPerSecond = (ahrs.getAngle() - startAngle)/(Timer.getFPGATimestamp() - startTime);
     }
 }
