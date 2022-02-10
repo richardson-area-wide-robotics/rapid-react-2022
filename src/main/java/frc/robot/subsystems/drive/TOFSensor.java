283714fc1b7a5198f5b0f170685a4e8d30e95bb9
Stuart <Rampy1745@gmail.com>
2022-02-10 14:46:28 -0600

    better declaration

--- src/main/java/frc/robot/subsystems/drive/TOFSensor.java
+++ src/main/java/frc/robot/subsystems/drive/TOFSensor.java
@@ -1,3 +1,5 @@
+package frc.robot.subsystems.drive;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DigitalSource;
 import edu.wpi.first.wpilibj.DutyCycle;
@@ -5,7 +7,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /*
 *Full data sheet https://drive.google.com/file/d/1TZaQEPTUOwr0a_-ehMSx1A6ZTU-sqt30/view?usp=sharing
-* PWM output:
+*from datasheet 
+*PWM output:
 *Another form of output data for the module is the PWM output, which has a square wave period of 20 Hz. The high level corresponds to the measured distance
 *Formula: Distance (mm) = High time (ms) * 100 = High time (us)/10
 *For example, if the measured high time is 10000us, then Distance=10000/10=1000mm
@@ -22,13 +25,10 @@ public class TOFSensor  extends SubsystemBase {
     public TOFSensor  (DigitalSource digitalSource) {
         this.port = new DutyCycle(digitalSource);
         this.targetDistance = 0;
-        this.setName("TOF Sensor port" + digitalSource.getPortHandleForRouting());
+        this.setName("TOF Sensor port: " + digitalSource.getPortHandleForRouting());
     }
     public TOFSensor (int PWMPort){ // declaration with an int for the PWM Port, This is mostlikely bad because you want the digital sources to be handled all at once somewhere else
-        DigitalSource digitalSource = new DigitalInput(PWMPort);
-        this.port = new DutyCycle(digitalSource);
-        this.targetDistance = 0;
-        this.setName("TOF Sensor port" + digitalSource.getPortHandleForRouting());
+       this(new DigitalInput(PWMPort));   
     }
 
     public void setTargetDistance(double targetDistance) {
